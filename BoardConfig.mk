9a58a8d6bd3f341f3418d9f3706b0a0f86cdcb2a
Jamie Falconer <mrfluffyoven@gmail.com>
2025-12-26 09:15:19 +0000

    Remove A/B OTA updater settings from BoardConfig
    Removed A/B OTA updater configuration and related settings.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,16 +10,6 @@ DEVICE_PATH := device/motorola/mona
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    product \
-    vendor \
-    system_ext \
-    system \
-    odm
-BOARD_USES_RECOVERY_AS_BOOT := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a

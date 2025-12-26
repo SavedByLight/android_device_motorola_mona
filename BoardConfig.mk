a54e8f356822e63de490ebd3f7b24f1f5013afe3
Jamie Falconer <mrfluffyoven@gmail.com>
2025-12-26 10:18:43 +0000

    Enable kernel by setting TARGET_NO_KERNEL to false

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_USES_VULKAN := true
 BOARD_VENDOR_CMDLINE += "video=vfb:640x400,bpp=32,memsize=3072000 nosoftlockup console=ttynull qcom_geni_serial.con_enabled=0 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image bootconfig"
 
 # Kernel
-TARGET_NO_KERNEL := true
+TARGET_NO_KERNEL := false
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_BOOT_HEADER_VERSION := 4

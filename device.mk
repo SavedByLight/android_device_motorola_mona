26d61950e4b697b6524bc3214bd2f4633565cc2f
Jamie Falconer <mrfluffyoven@gmail.com>
2026-02-05 06:49:30 +0000

    REWORK: recovery.img

--- device.mk
+++ device.mk
@@ -11,9 +11,6 @@ LOCAL_PATH := device/motorola/mona
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
 
